2a4ba238e69bad8a7d9b9fc3319246ff870f5646
Khoi Le <le_k2@denison.edu>
2020-06-16 13:51:55 -0400

    Update the max value to be one (x-coordinate)

--- src/Python/general_lowerbound.py
+++ src/Python/general_lowerbound.py
@@ -2,11 +2,13 @@ import os
 import subprocess
 import matplotlib.pyplot as pyplot
 from mpl_toolkits.mplot3d import Axes3D
+import itertools
+import math
 
 def lowerbound(k, d):
     f = open("dD_lowerbound.py", "r+", encoding="utf-8")
     f.truncate(0)
-    f.write("import gurobipy as gp\nfrom gurobipy import GRB\n\n")
+    f.write("import gurobipy as gp\nfrom gurobipy import GRB\nimport math\n\n")
     f.write("m = gp.Model(\"qp\")\n\n")
     f.write("x = m.addVar(lb= 0, ub= 1, vtype=GRB.CONTINUOUS, name=\"x\")\n\n")
     for i in range (1,k+2):
@@ -19,12 +21,26 @@ def lowerbound(k, d):
             f.write("p"+str(i)+str(j)+" = m.addVar(lb= 0, ub= 1, vtype=GRB.CONTINUOUS, name=\"p"+str(i)+str(j)+"\")\n")
         f.write("\n")
     f.write("\n\n")
-    f.write("m.setObjective(x, GRB.MAXIMIZE)\n\n\n")
+    f.write("m.setObjective(x, GRB.MAXIMIZE)\n\n")
+
+    f.write("m.addConstr(p" + str(k + 1) + "1==1, \"res1\")")
+
+    f.write("\n")
 
     for i in range (1, k+1):
         f.write("m.addConstr(p"+str(i)+"1<=p"+str(i+1)+"1, \"c0"+str(i)+"\")\n")
 
-    f.write("\n\n")
+    f.write("\n")
+
+    # Test for excluding dominance
+    # l = [i for i in range (1, k+2)]
+    # for i in itertools.combinations(l, 2):
+    #     f.write("m.addConstr(math.ceil(p"+str(i[0])+str(1)+"-p"+str(i[1])+str(1)+")")
+    #     for j in range (2,d+1):
+    #         f.write("*math.ceil(p" + str(i[0]) + str(j) + "-p" + str(i[1]) + str(j) + ")")
+    #     f.write("==0, \"dom"+str(i[0])+str(i[1])+"\")\n")
+
+    f.write("\n")
 
     for i in range (1,k+2):
         f.write("m.addConstr(p"+str(i)+str(1)+"*v"+str(i)+str(1))
@@ -55,6 +71,7 @@ def lowerbound(k, d):
     return subprocess.check_output("gurobi.bat dD_lowerbound.py", shell=True)
 
 def graph2d(k, d):
+    print("\n")
     p = lowerbound(k, d)
 
     p = p.decode()
@@ -64,11 +81,13 @@ def graph2d(k, d):
     points = []
     for i in p:
         if len(i) != 0 and i[0] == "x":
-            print("Max x = "+i[2:])
+            print("Max x = " + i[2:])
         if len(i) != 0 and i[0] == "p":
-            points.append(float(i[4:]))
+            point = i.split()
+            points.append(float(point[1]))
         if len(i) != 0 and i[0] == "v":
-            v.append(float(i[4:]))
+            vector = i.split()
+            v.append(float(vector[1]))
 
     x = []
     y = []
@@ -101,6 +120,7 @@ def graph2d(k, d):
     pyplot.show()
 
 def graph3d(k, d):
+    print("\n")
     p = lowerbound(k, d)
 
     p = p.decode()
@@ -110,11 +130,13 @@ def graph3d(k, d):
     points = []
     for i in p:
         if len(i) != 0 and i[0] == "x":
-            print("Max x = "+i[2:])
+            print("Max x = " + i[2:])
         if len(i) != 0 and i[0] == "p":
-            points.append(float(i[4:]))
+            point = i.split()
+            points.append(float(point[1]))
         if len(i) != 0 and i[0] == "v":
-            v.append(float(i[4:]))
+            vector = i.split()
+            v.append(float(vector[1]))
 
     x = []
     y = []
@@ -139,9 +161,9 @@ def graph3d(k, d):
     maxy = max(y)
     maxz = max(z)
     for i in range (len(x)):
-        x[i] = x[i]/maxx
+        x[i] = x[i]/(maxx)
         y[i] = y[i]/maxy
-        z[i] = z[i]/maxz
+        z[i] = z[i]/(maxz)
 
     print(x)
     print(y)
